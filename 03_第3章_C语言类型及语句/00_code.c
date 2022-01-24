10b84cc40f3c80c911a476c7b9efe6f514ac87db
Chenxuwkk <1814300923@qq.com>
2022-01-24 19:14:18 +0800

    Update 00_code.c
    测试提交

--- 03_第3章_C语言类型及语句/00_code.c
+++ 03_第3章_C语言类型及语句/00_code.c
@@ -17,7 +17,7 @@
 
 //头文件 是对 库函数 进行声明
 #include <stdio.h>//标准输入输出头文件
-
+#include <string.h>//
 //main 就是主函数 一个工程有且只能有一个主函数
 //main 是程序调用的入口
 //main左边的int 为函数返回值类型 （）为函数的形参 
@@ -26,7 +26,6 @@ int main(int argc, char *argv[])
 {//函数体
 	//printf将()里面的字符串输出到 终端上
 	printf("hello world\n");//分号;是结束语句
-
 	//retrun 第一：返回值函数值   第二:结束当前函数
 	return 0;
 }
